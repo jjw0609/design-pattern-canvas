7a6f0e1bf8ed34d010c8b0b82a8f227df76ea8d0
jjw0609 <jjw_0609@naver.com>
2025-08-25 22:35:05 +0900

    프로토타입(Prototype) - 기존 객체를 복붙해서 새 객체 만들기

--- src/GrimpanHistory.ts
+++ src/GrimpanHistory.ts
@@ -1,13 +1,34 @@
 import type Grimpan from "./AbstractGrimpan.js";
 import type IEGrimpan from "./IEGrimpan.js";
 import type ChromeGrimpan from "./ChromeGrimpan.js";
+import {ChromeGrimpanMenu, IEGrimpanMenu} from "./GrimpanMenu.js";
 
+
+interface Clonable {
+    clone(): Clonable;
+}
+
+class HistoryStack extends Array implements Clonable {
+    clone() {
+        return this.slice() as HistoryStack;
+    }
+}
 
 export abstract class GrimpanHistory {
     grimpan: Grimpan;
+    stack: HistoryStack;
+
+    getStack() {
+        return this.stack.clone();
+    }
 
     protected constructor(grimpan: Grimpan) {
         this.grimpan = grimpan;
+        this.stack = new HistoryStack();
+    }
+
+    setStack(stack: HistoryStack) {
+        this.stack = stack.clone();
     }
 
     abstract initialize(): void
